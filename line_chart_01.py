88f9f5aba4bb8285512ff35fa9300b4365d019fa
Mariana <marianas.medeiros@yahoo.com.br>
2019-12-03 22:35:25 -0300

    line_chart_01

---
+++ line_chart_01.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+
+# podemos fazer multiplas chamadas para plt.plot
+# para mostrar multiplas series no mesmo grafico
+
+plt.plot(xs, variance, 'g-', label='variance') # Linha verde s[olida
+plt.plot(xs, bias_squared, 'r-', label='bias^2') # linha com linha de ponto tracejado vermelho
+plt.plot(xs, total_error, 'b:', label='total error') # linha com pontilhado azul
+
+# porque atribuimos rotulos para cada serie
+# podemos obter uma legenda gratuita
+# loc = 9 significa "top center"
+plt.legend(loc=9)
+plt.xlabel("complexidade do modelo")
+plt.title("Compromisso entre Polarização e Variância")
+plt.savefig("line_chart_01.png")
